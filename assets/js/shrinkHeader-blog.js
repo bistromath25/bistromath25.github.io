daf1d428fb83a96b94c0320a7d1767d8e7ce2b56
bistromath25 <71907421+bistromath25@users.noreply.github.com>
2022-06-20 15:36:21 -0400

    Add files via upload

---
+++ assets/js/shrinkHeader-blog.js
@@ -0,0 +1,12 @@
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+ 	if (document.body.scrollTop > screen.height * 0.5 || document.documentElement.scrollTop > screen.height * 0.5) {
+		document.getElementById("Header").style.padding = "0px";
+		document.getElementById("Header").style.fontSize = "0.8em";
+		document.getElementById("Header").style.opacity = "0.9";
+	} else {
+    	document.getElementById("Header").style.padding = "10px";
+		document.getElementById("Header").style.fontSize = "1em";
+		document.getElementById("Header").style.opacity = "0.8";
+	}
+}
