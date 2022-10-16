e67f2c1bee24442e599dc20244d818adf7262b0b
bistromath25 <71907421+bistromath25@users.noreply.github.com>
2022-10-15 22:03:13 -0400

    Make background image fixed

--- assets/js/shrinkHeader.js
+++ assets/js/shrinkHeader.js
@@ -1,12 +1,14 @@
 window.onscroll = function () { scrollFunction() };
 function scrollFunction() {
-    if (document.body.scrollTop > screen.height * 0.7 || document.documentElement.scrollTop > screen.height * 0.7) {
+    if (document.body.scrollTop > screen.height * 0.8 || document.documentElement.scrollTop > screen.height * 0.8) {
         document.getElementById("Header").style.padding = "0px";
         document.getElementById("Header").style.fontSize = "0.8em";
         document.getElementById("Header").style.opacity = "0.9";
+		document.getElementById("About").style.height = "9vh";
     } else {
         document.getElementById("Header").style.padding = "10px";
         document.getElementById("Header").style.fontSize = "1em";
         document.getElementById("Header").style.opacity = "0.4";
+		document.getElementById("About").style.height = "3vh";
     }
 }
