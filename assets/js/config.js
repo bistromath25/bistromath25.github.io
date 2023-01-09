43c4c74da7f4643b550c552c2d1435c69ff67cd1
Brighten Zhang <bistromath25@gmail.com>
2023-01-08 19:00:31 -0500

    major updates

---
+++ assets/js/config.js
@@ -0,0 +1,6 @@
+// Replace
+function loadConfig() {
+	document.getElementById("GH_PAGE_NAME").innerHTML = "bistromath25.github.io";
+	document.getElementById("COPYRIGHT").innerHTML = "&copy; Brighten Zhang";
+	document.getElementById("EMAIL").innerHTML = "&bull; <a href='mailto:brightenzhang@gmail.com'>Email me!</a> &bull;";
+};
--- assets/js/shrinkHeader-test.js
+++ assets/js/shrinkHeader-test.js
@@ -4,9 +4,11 @@ function scrollFunction() {
         document.getElementById("Header").style.padding = "0px";
         document.getElementById("Header").style.fontSize = "0.8em";
         document.getElementById("Header").style.opacity = "0.9";
+		document.getElementById("About").style.height = "9vh";
     } else {
         document.getElementById("Header").style.padding = "10px";
         document.getElementById("Header").style.fontSize = "1em";
         document.getElementById("Header").style.opacity = "0.4";
+		document.getElementById("About").style.height = "3vh";
     }
 }
