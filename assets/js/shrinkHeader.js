b3b578705ee2aae32bc22497c53f3f6b57a4e7aa
Brighten Zhang <bistromath25@gmail.com>
2023-01-12 17:11:39 -0500

    background image, title animation

--- assets/js/shrinkHeader.js
+++ assets/js/shrinkHeader.js
@@ -6,10 +6,8 @@ function scrollFunction() {
     if (document.body.scrollTop > screen.height * 0.8 || document.documentElement.scrollTop > screen.height * 0.8) {
         document.getElementById("Header").style.padding = "0px";
         document.getElementById("Header").style.fontSize = "0.8em";
-        document.getElementById("Header").style.opacity = "0.9";
     } else {
         document.getElementById("Header").style.padding = "10px";
         document.getElementById("Header").style.fontSize = "1em";
-        document.getElementById("Header").style.opacity = "0.4";
-    }
+	}
 }
